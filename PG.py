dc0fed3621839c9e5cf204f395125105f83c87bd
Mohamed Islam Mekadmini <40797126+medislamm123@users.noreply.github.com>
2018-07-04 00:24:19 +0100

    Add files via upload

--- PG.py
+++ PG.py
@@ -1,6 +1,21 @@
 import random
-j=1
-while j!=0:
+j=1 #this is a counter that enables the script to display a welcoming message.
+#------------------------------------------------------------------------------------
+def view():
+    file = open('password_generator.txt','r+')
+    read = file.read()
+    if read != '':
+        print(f"""-----------------------------------------------
+Here are your passwords:
+
+{read}
+
+-----------------------------------------------""")
+    else:
+        print('No passwords were saved')
+    file.close()
+#------------------------------------------------------------------------------------
+while True:
     if j==1:
         print("""
 -----------------------------------------------
@@ -13,18 +28,28 @@ Welcome to the Password Generator!""")
 \t R : Repair the program.
 \t E : Exit.
 -----------------------------------------------""")
-
-    answer = input('Your answer is:\n \t=>  ')
+    a  = input('Your answer is:\n \t=>  ')
+    answer = a.upper()
 #------------------------------------------------------------------------------------
     if answer == 'G':
         password = ''
-        number_of_characters = input('N° of characters ?\n \t=>  ')
+
+        while True:
+            try:
+                number_of_characters = int(input('N° of characters ?\n \t=>  '))
+                break
+            except ValueError :
+                print('Please write an integer!')
+
+
         print("""Type of characters?
 \t L : Letters and symbols.
 \t N : Numbers.
 \t R or [Any Key] : random.
 -----------------------------------------------""")
-        type_of_characters = input('\t=>  ')
+        type_ch = input('\t=>  ')
+        type_of_characters = type_ch.upper()
+
         if type_of_characters == 'L':
             a = 58
             b = 126
@@ -34,11 +59,11 @@ Welcome to the Password Generator!""")
         else:
             a = 33
             b = 126
-        for i in range(int(number_of_characters)):
+        for i in range(number_of_characters):
             password = password +  chr(random.randint(a,b))
         print(f"Please copy your password:\n \t=>  {password} \n ")
         yes_or_no = input('Should I save your password ?\n \t=>  ')
-        if yes_or_no == 'yes':
+        if yes_or_no.upper() == 'YES':
             account = input('To which account is this password assigned ?\n \t=>  ')
             file = open('password_generator.txt','r+')
             saved= file.read()
@@ -47,19 +72,13 @@ Welcome to the Password Generator!""")
             else:
                 file.write('\n'+account+': '+password)
             file.close()
+            view_my_passwords = input('Do you want to view your saved passwords ?\n \t=>  ')
+            if view_my_passwords.upper() == 'YES':
+                view()
+
+
     elif answer == 'V':
-        file = open('password_generator.txt','r+')
-        read = file.read()
-        if read != '':
-            print(f"""-----------------------------------------------
-Here are your passwords:
-
-{read}
-
------------------------------------------------""")
-        else:
-            print('No passwords were saved')
-        file.close
+        view()
     elif answer == 'N':
         my_password = input("What's your password ?\n \t=>  ")
         account = input('To which account is this password assigned ?\n \t=>  ')
@@ -73,18 +92,18 @@ Here are your passwords:
     elif answer =='R':
         print(' Warning!\n \tResetting the program will result in deleting saved passwords.')
         verification = input('Should you wish to continue ?\n \t=>  ')
-        if verification == 'yes':
+        if verification.upper() == 'YES':
             file = open('password_generator.txt','w+')
             file.truncate(0)
             file.close()
             print('All saved passwords were deleted.')
-        elif verification =='no':
+        elif verification.upper() =='NO':
             print('No files were deleted.')
         else:
             print(" The program doesn't recognize your command.")
     elif answer =='E':
         print('Goodbye!')
-        j=0
+        break
     else:
         print(" The program doesn't recognize your command.")
 
@@ -92,13 +111,17 @@ Here are your passwords:
     if answer !='E':
             Exit = ''
             message = 0
+
             while Exit !='E'and Exit!='C':
-                if message!=0:
+                if message!=0: #this message is only displayed if the user doesn't type correctly
                     print(" The program doesn't recognize your command.")
-                Exit = input('Press E to Exit or C to continue.\n \t=>  ')
-                message = 1
+                e = input('Press E to Exit or C to continue.\n \t=>  ')
+                Exit = e.upper()
+                message = 1 #message changes but isn't displayed unless the Exit is different from 'E' or 'C'
+
+
             if Exit =='E':
                 print('Goodbye!')
-                j=0
+                break
             else:
                 j=j+1
